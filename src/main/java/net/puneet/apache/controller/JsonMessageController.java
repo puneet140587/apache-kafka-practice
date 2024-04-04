a7ef8ddf67023474da02deee61e5c839b1ae9f93
Puneet Shrivastava <Puneet.shrivastava@telus.com>
2024-04-04 15:48:26 +0530

    Just added a comment

--- src/main/java/net/puneet/apache/controller/JsonMessageController.java
+++ src/main/java/net/puneet/apache/controller/JsonMessageController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/kafka")
 public class JsonMessageController {
 
+    // This is kafka message controller
+
     private JsonKafkaProducer kafkaProducer;
 
     public JsonMessageController(JsonKafkaProducer kafkaProducer) {
